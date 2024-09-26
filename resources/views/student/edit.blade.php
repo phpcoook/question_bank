28289cc9df298ffeef91fb336b2276a44138eeba
Kapadi-Hardik <imbhavin95@gmail.com>
2024-09-26 13:05:25 +0530

    change empty question msg and add std to student crud

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -38,7 +38,27 @@
                     @csrf
                     @method('PUT')
                     <div class="card-body">
-
+                        <div class="form-group">
+                            <label for="std">Standard</label>
+                            <select name="std" class="form-control" required>
+                                <option value="">Select Standard</option>
+                                <option value="1" {{ ($data->std == 1) ? 'selected' : '' }}>1<sup>st</sup></option>
+                                <option value="2" {{ ($data->std == 2) ? 'selected' : '' }}>2<sup>nd</sup></option>
+                                <option value="3" {{ ($data->std == 3) ? 'selected' : '' }}>3<sup>rd</sup></option>
+                                <option value="4" {{ ($data->std == 4) ? 'selected' : '' }}>4<sup>th</sup></option>
+                                <option value="5" {{ ($data->std == 5) ? 'selected' : '' }}>5<sup>th</sup></option>
+                                <option value="6" {{ ($data->std == 6) ? 'selected' : '' }}>6<sup>th</sup></option>
+                                <option value="7" {{ ($data->std == 7) ? 'selected' : '' }}>7<sup>th</sup></option>
+                                <option value="8" {{ ($data->std == 8) ? 'selected' : '' }}>8<sup>th</sup></option>
+                                <option value="9" {{ ($data->std == 9) ? 'selected' : '' }}>9<sup>th</sup></option>
+                                <option value="10" {{ ($data->std == 10) ? 'selected' : '' }}>10<sup>th</sup></option>
+                                <option value="11" {{ ($data->std == 11) ? 'selected' : '' }}>11<sup>th</sup></option>
+                                <option value="12" {{ ($data->std == 12) ? 'selected' : '' }}>12<sup>th</sup></option>
+                            </select>
+                            @error('std')
+                            <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
                         <div class="form-group">
                             <label for="first_name">First Name</label>
                             <input type="text" name="first_name" id="first_name" class="form-control"
@@ -131,6 +151,9 @@
                     date_of_birth: {
                         required: true,
                         date: true
+                    },
+                    std: {
+                        required: true
                     }
                 },
                 messages: {
@@ -155,6 +178,9 @@
                     date_of_birth: {
                         required: "Please select the date of birth",
                         date: "Please enter a valid date"
+                    },
+                    std: {
+                        required: "Please select the Standard"
                     }
                 },
                 errorElement: 'div',
