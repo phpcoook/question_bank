3d89f9cc4d2391b25750eb2b832be2534a399141
Bhavin Sasapra <imbhavin95@gmail.com>
2024-12-05 19:39:00 +0530

    add live logo

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('question.index')}}" class="brand-link">
-        <img src="{{ url('assets/plugins/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+        <img src="{{ url('assets/plugins/dist/img/logo.png') }}" alt="AdminLTE Logo"
              class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">Quiz</span>
